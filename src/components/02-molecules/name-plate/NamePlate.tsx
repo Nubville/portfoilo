02c5b292bb1a6b2fbb276aa0b76453b92235f2d8
Drew Garman <dgarman@geekhive.com>
2022-02-28 07:26:40 -0500

    Added date to nameplate component

--- src/components/02-molecules/name-plate/NamePlate.tsx
+++ src/components/02-molecules/name-plate/NamePlate.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { space, SpaceProps } from 'styled-system';
+import theme from '../../00-base/01-theme/theme';
 import { ThemeType } from '../../00-base/01-theme/theme.type';
 
 export interface BaseNamePlateProps {
@@ -15,7 +16,13 @@ const Details: React.FC = styled.p`
   margin: ${(props) => props.theme.space.none};
 `;
 
+const TopDetailsWrapper: React.FC = styled.div`
+  margin-bottom: ${theme.space.x3}px;
+`;
+
 const StyledNamePlate: React.FC<NamePlateProps> = styled.div`
+  display: flex;
+  flex-direction: column;
   flex: 1 0 100%;
   margin-top: ${(props) => props.theme.space.x2}px;
   text-align: right;
@@ -23,12 +30,19 @@ const StyledNamePlate: React.FC<NamePlateProps> = styled.div`
 `;
 
 export const NamePlate: React.FC<NamePlateProps> = ({ children, ...props }) => {
+  const currentDate = new Date();
+  const date = `${
+    currentDate.getMonth() + 1
+  }/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+
   return (
     <StyledNamePlate {...props}>
-      <Details>Andrew Garman</Details>
-      <Details>Fort Wayne, IN</Details>
-      <a href="mailto:garman.andrew@gmail.com">Email</a>
-      {children}
+      <TopDetailsWrapper>
+        <Details>Andrew Garman</Details>
+        <Details>Fort Wayne, IN</Details>
+        <a href="mailto:garman.andrew@gmail.com">Email</a>
+      </TopDetailsWrapper>
+      <Details>{date}</Details>
     </StyledNamePlate>
   );
 };
